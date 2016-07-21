04ff0b2c887092e3a19018002b2e0cc226de85b7
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2016-07-21 13:45:39 -0500

    Fix object name

--- app/controllers/spree/email_sender_controller_decorator.rb
+++ app/controllers/spree/email_sender_controller_decorator.rb
@@ -1,4 +1,28 @@
 Spree::EmailSenderController.class_eval do
+  private
+
+  def mail_to_friend
+    @mail_to_friend = Spree::MailToFriend.new(params[:mail_to_friend])
+    @mail_to_friend.host = request.env['HTTP_HOST']
+    respond_to do |format|
+      format.html do
+        captcha_passed = !Spree::Captcha::Config[:use_captcha] || verify_recaptcha(private_key: Spree::Captcha::Config[:private_key])
+        if @mail_to_friend.valid? && captcha_passed
+          flash[:notice] = Spree.t('email_to_friend.mail_sent_to', email: @mail_to_friend.recipients.join(', '))
+
+          send_message(@wishlist, @mail_to_friend)
+
+          method_name = "after_delivering_#{@wishlist.class.name.downcase}_mail"
+          send(method_name) if respond_to?(method_name, true)
+
+          redirect_to @wishlist
+        else
+          render action: :send_mail
+        end
+      end
+    end
+  end
+
   def find_object
     class_name = "Spree::#{(params[:type].titleize)}".constantize
     return false if params[:id].blank?
